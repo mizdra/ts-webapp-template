527db31b89045be1e5ebb431024e9c854598e2dd
mizdra <pp.mizdra@gmail.com>
2026-02-27 01:44:10 +0900

    enable next.js options

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  cacheComponents: true,
+  typedRoutes: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
